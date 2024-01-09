caa5956b1ee1aa626fd6dd308827173ccfde9deb
Paweł Żurakowski <55536129+sommbre@users.noreply.github.com>
2024-01-09 16:45:32 +0100

    Add support for adding/removing containers

--- machine_manager/src/docker_wrapper.py
+++ machine_manager/src/docker_wrapper.py
@@ -1,8 +1,42 @@
 import docker
 
+class Image():
+    def __init__(self, lang, version, app_port=80, env=None):
+        self.lang = lang
+        self.version = version
+        self.app_port = app_port
+        self.env = env
+
+
+class Container():
+    def __init__(self, id, lang, version, host_port):
+        self.id = id
+        self.lang = lang
+        self.version = version
+        self.host_port = host_port
+
+
 class DockerWrapper():
     def __init__(self):
         self.client = docker.from_env()
 
+    def create(self, image):
+        raw_container = self.client.containers.run(
+            image=f'{image.lang}:{image.version}',
+            ports={f'{image.app_port}/tcp': None},
+            environment=image.env,
+            detach=True
+        )
+
+        # Needed to refresh attributes such as host port
+        # See https://docker-py.readthedocs.io/en/stable/containers.html#docker.models.containers.Container
+        raw_container.reload()
+        host_port = raw_container.attrs['NetworkSettings']['Ports'][f'{image.app_port}/tcp'][0]['HostPort']
+
+        return Container(raw_container.id, image.lang, image.version, host_port)
+
+    def remove(self, container):
+        self.client.containers.get(container.id).remove(force=True)
+
     def list(self):
-        return map(lambda c: c.name, self.client.containers.list())
+        pass
