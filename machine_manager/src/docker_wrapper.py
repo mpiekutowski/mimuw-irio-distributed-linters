e76cd49fda5190726d8b4505663c18e0304079e9
Paweł Żurakowski <55536129+sommbre@users.noreply.github.com>
2024-01-16 21:48:58 +0100

    Refactor DockerWrapper and sibling classes

--- machine_manager/src/docker_wrapper.py
+++ machine_manager/src/docker_wrapper.py
@@ -1,56 +1,55 @@
 import docker
+from dataclasses import dataclass
 
 class DockerError(Exception):
     def __init__(self, message="Docker operation failed"):
         super().__init__(message)
 
+@dataclass
 class Image():
-    def __init__(self, name, version, app_port=80, env=None):
-        self.name = name
-        self.version = version
-        self.app_port = app_port
-        self.env = env
+    name: str
+    app_port: int
+    env: dict
 
 
+@dataclass
 class Container():
-    def __init__(self, id, lang, version, host_port):
-        self.id = id
-        self.lang = lang
-        self.version = version
-        self.host_port = host_port
+    id: str
+    host_port: int
 
 
 class DockerWrapper():
     def __init__(self):
         self.client = docker.from_env()
 
-    def create(self, image):
+    def create(self, image: Image):
         try:
             raw_container = self.client.containers.run(
-                image=f'{image.name}:{image.version}',
-                ports={f'{image.app_port}/tcp': None},
+                image=image.name,
+                ports={f'{image.app_port}/tcp': None}, # None means assign random free port on host
                 environment=image.env,
                 detach=True
             )
 
-            # Needed to refresh attributes such as host port
+            # Reload is needed to get access to attributes like host port
             # See https://docker-py.readthedocs.io/en/stable/containers.html#docker.models.containers.Container
             raw_container.reload()
             host_port = raw_container.attrs['NetworkSettings']['Ports'][f'{image.app_port}/tcp'][0]['HostPort']
+            print(raw_container.attrs['NetworkSettings'].keys())
 
-            return Container(raw_container.id, image.env["LANGUAGE"], image.version, int(host_port))
-        except docker.errors.ImageNotFound as e:
+            return Container(id=raw_container.id, host_port=int(host_port))
+        except docker.errors.ImageNotFound:
             raise DockerError(f'Container image not found')
-        except docker.errors.APIError as e:
+        except docker.errors.APIError:
             raise DockerError(f'Internal docker error')
-        except docker.errors.ContainerError as e:
+        except docker.errors.ContainerError:
             raise DockerError(f'Container exited with non-zero code')
 
-    def remove(self, container, timeout):
+    def remove(self, container: Container, timeout: int):
         try:
             self.client.containers.get(container.id).stop(timeout=timeout)
             self.client.containers.get(container.id).remove()
-        except docker.errors.NotFound as e:
+        except docker.errors.NotFound:
             raise DockerError(f'Could not find container')
-        except docker.errors.APIError as e:
+        except docker.errors.APIError:
             raise DockerError(f'Internal docker error')
