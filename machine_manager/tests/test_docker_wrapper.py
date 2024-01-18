0d07ebdaca2178e115329d30e3694e986654f241
Paweł Żurakowski <55536129+sommbre@users.noreply.github.com>
2024-01-18 22:15:29 +0100

    Better tests for docker wrapper

--- machine_manager/tests/test_docker_wrapper.py
+++ machine_manager/tests/test_docker_wrapper.py
@@ -3,30 +3,25 @@ import docker.errors
 import pytest
 from src.docker_wrapper import DockerWrapper, DockerError, Image, Container
 
-class MockClient:
-    def __init__(self, networks, containers):
-        self.networks = networks
-        self.containers = containers
+@pytest.fixture()
+def mock_client():
+    client = Mock()
+    
+    client.networks.get.return_value = Mock()
+    client.networks.get.return_value.name = 'linter_network'
 
-@pytest.fixture
-def throwing_networks():
-    mock_networks = Mock()
-    mock_networks.get.side_effect = docker.errors.NotFound("some message")
-    return mock_networks
+    return client
 
-@pytest.fixture
-def well_behaved_networks():
-    mock_networks = Mock()
-    mock_specific_network = Mock()
-    mock_specific_network.name = "linter_network"
+def test_network_not_found():
+    client = Mock()
+    client.networks.get.side_effect = docker.errors.NotFound('Network not found')
 
-    mock_networks.get.return_value = mock_specific_network
-    return mock_networks
+    with patch('docker.from_env', return_value=client):
+        with pytest.raises(DockerError):
+            DockerWrapper()
 
-@pytest.fixture
-def mock_container():
+def test_run_success(mock_client):
     mock_container = Mock()
-    mock_container.id = "some_id"
     mock_container.attrs = {
         "NetworkSettings": {
             "Networks": {
@@ -36,69 +31,89 @@ def mock_container():
             }
         }
     }
-    return mock_container
-
-@pytest.fixture
-def image():
-    return Image(name="some_name", app_port=1234, env={"some": "env"})
-
-def test_network_not_found(throwing_networks):
-    with patch("docker.from_env", return_value=MockClient(throwing_networks, Mock())):
-        with pytest.raises(DockerError):
-            DockerWrapper()
-
-def test_run_successful(image, mock_container, well_behaved_networks):
-    mock_client = Mock()
-    mock_client.containers = Mock()
+    mock_container.id = "some_id"
     mock_client.containers.run.return_value = mock_container
 
-    mock_client.networks = well_behaved_networks
-
-    with patch("docker.from_env", return_value=mock_client):
+    with patch('docker.from_env', return_value=mock_client):
         docker_wrapper = DockerWrapper()
+
+        image = Image(name='image_name', app_port=1234, env={"some": "env"})
         container = docker_wrapper.create(image)
 
-        assert container == Container(id="some_id", address="172.0.0.1:1234")
+        assert container.address == f"172.0.0.1:{image.app_port}"
+        assert container.id == "some_id"
 
-        mock_client.containers.run.assert_called_once_with(
+        mock_client.containers.run.assert_called_with(
             image=image.name,
             environment=image.env,
             network=mock_client.networks.get.return_value.name,
             detach=True,
         )
 
-def test_run_image_not_found(image, well_behaved_networks):
-    mock_client = Mock()
-    mock_client.containers = Mock()
-    mock_client.containers.run.side_effect = docker.errors.ImageNotFound("some message")
+def test_run_api_error(mock_client):
+    mock_client.containers.run.side_effect = docker.errors.APIError('API error')
 
-    mock_client.networks = well_behaved_networks
-
-    with patch("docker.from_env", return_value=mock_client):
+    with patch('docker.from_env', return_value=mock_client):
         docker_wrapper = DockerWrapper()
+
+        image = Image(name='image_name', app_port=1234, env={"some": "env"})
+
         with pytest.raises(DockerError):
             docker_wrapper.create(image)
 
-def test_run_api_error(image, well_behaved_networks):
-    mock_client = Mock()
-    mock_client.containers = Mock()
-    mock_client.containers.run.side_effect = docker.errors.APIError("some message")
 
-    mock_client.networks = well_behaved_networks
+def test_run_not_found(mock_client):
+    mock_client.containers.run.side_effect = docker.errors.ImageNotFound('Image not found')
 
-    with patch("docker.from_env", return_value=mock_client):
+    with patch('docker.from_env', return_value=mock_client):
         docker_wrapper = DockerWrapper()
+
+        image = Image(name='image_name', app_port=1234, env={"some": "env"})
+
         with pytest.raises(DockerError):
             docker_wrapper.create(image)
 
-def test_run_container_error(image, well_behaved_networks):
-    mock_client = Mock()
-    mock_client.containers = Mock()
-    mock_client.containers.run.side_effect = docker.errors.ContainerError(None, None, None, None, None) 
+def test_run_container_error(mock_client):
+    mock_client.containers.run.side_effect = docker.errors.ContainerError(None, None, None, None, None)
 
-    mock_client.networks = well_behaved_networks
-
-    with patch("docker.from_env", return_value=mock_client):
+    with patch('docker.from_env', return_value=mock_client):
         docker_wrapper = DockerWrapper()
+
+        image = Image(name='image_name', app_port=1234, env={"some": "env"})
+
         with pytest.raises(DockerError):
             docker_wrapper.create(image)
+
+def test_remove_success(mock_client):
+    mock_client.containers.get.return_value.stop.return_value = None
+
+    with patch('docker.from_env', return_value=mock_client):
+        docker_wrapper = DockerWrapper()
+        container = Container(id="some_id", address="172.0.0.1:80")
+        docker_wrapper.remove(container, timeout=1)
+
+        mock_client.containers.get.assert_called_with(container.id)
+        mock_client.containers.get.return_value.stop.assert_called_with(timeout=1)
+        mock_client.containers.get.return_value.remove.assert_called_with()
+
+
+
+def test_remove_api_error(mock_client):
+    mock_client.containers.get.return_value.stop.side_effect = docker.errors.APIError('API error')
+
+    with patch('docker.from_env', return_value=mock_client):
+        docker_wrapper = DockerWrapper()
+        container = Container(id="some_id", address="172.0.0.1:80")
+
+        with pytest.raises(DockerError):
+            docker_wrapper.remove(container, timeout=1)
+
+def test_remove_not_found(mock_client):
+    mock_client.containers.get.return_value.stop.side_effect = docker.errors.NotFound('Container not found')
+
+    with patch('docker.from_env', return_value=mock_client):
+        docker_wrapper = DockerWrapper()
+        container = Container(id="some_id", address="172.0.0.1:80")
+
+        with pytest.raises(DockerError):
+            docker_wrapper.remove(container, timeout=1)
