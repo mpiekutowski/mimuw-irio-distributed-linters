33968a912094fb90c70ff153fed1e0e6234d3f9b
Miłosz Piekutowski <44316387+mpiekutowski@users.noreply.github.com>
2024-01-22 20:19:25 +0100

    Add Uris unit test (#41)

--- load_balancer/src/test/scala/loadbalancer/domain/UrisTest.scala
+++ load_balancer/src/test/scala/loadbalancer/domain/UrisTest.scala
@@ -3,13 +3,15 @@ package loadbalancer.domain
 import munit.FunSuite
 import org.http4s.Uri
 class UrisTest extends FunSuite {
-  test("currentOpt should return first element") {
+  private val backendKind = BackendKind("java", "1.0")
+
+  test("currentOpt should return first element of kind") {
     // given
     val uris     = getSampleUris(1, 3)
     val expected = Uri.unsafeFromString("http://localhost:8081")
 
     // when
-    val obtained = uris.currentOpt.get
+    val obtained = uris.currentOpt(backendKind).get
 
     // then
     assertEquals(obtained, expected)
@@ -21,13 +23,13 @@ class UrisTest extends FunSuite {
     val expected = None
 
     // when
-    val obtained = uris.currentOpt
+    val obtained = uris.currentOpt(backendKind)
 
     // then
     assertEquals(obtained, expected)
   }
 
-  test("remove should drop first element") {
+  test("remove should remove specified element from kind") {
     // given
     val uris     = getSampleUris(1, 3)
     val expected = getSampleUris(2, 3)
@@ -39,19 +41,38 @@ class UrisTest extends FunSuite {
     assertEquals(obtained, expected)
   }
 
-  test("add should append next element") {
+  test("add should add specified element to kind") {
     // given
     val uris     = getSampleUris(1, 2)
     val expected = getSampleUris(1, 3)
 
     // when
-    val obtained = uris.add(Uri.unsafeFromString("http://localhost:8083"))
+    val obtained = uris.add(backendKind, Uri.unsafeFromString("http://localhost:8083"))
 
     // then
     assertEquals(obtained, expected)
   }
 
-  private def getSampleUris(from: Int, to: Int): Uris = Uris {
-    (from to to).map(i => Uri.unsafeFromString(s"http://localhost:808$i")).toVector
+  test("spin should move first element to last position") {
+    // given
+    val uris = getSampleUris(1, 4)
+    val expected =
+      Uris(
+        Map(
+          backendKind -> Vector(getSampleUri(2), getSampleUri(3), getSampleUri(4), getSampleUri(1))
+        )
+      )
+
+    // when
+    val obtained = uris.spin(backendKind)
+
+    // then
+    assertEquals(obtained, expected)
   }
+
+  private def getSampleUris(from: Int, to: Int): Uris =
+    Uris(Map(backendKind -> (from to to).map(getSampleUri).toVector))
+
+  private def getSampleUri(i: Int): Uri =
+    Uri.unsafeFromString(s"http://localhost:808$i")
 }
